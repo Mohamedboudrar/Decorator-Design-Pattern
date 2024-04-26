f92a6bad695646862dc5e81b16816c6dd846d266
Mohamed Boudrar <boudrarmed2003@gmail.com>
2024-04-26 11:15:59 +0100

    Decorator

---
+++ After_decorator_design_pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class IceCream
+{
+    public virtual string Description => "Plain Ice Cream";
+    public virtual decimal CalculateCost() => 2.0m;
+}
+class IceCreamDecorator : IceCream
+{
+    protected IceCream iceCream;
+
+    public IceCreamDecorator(IceCream iceCream)
+    {
+        this.iceCream = iceCream;
+    }
+    public override string Description => iceCream.Description;
+    public override decimal CalculateCost() => iceCream.CalculateCost();
+}
+class Sprinkles : IceCreamDecorator
+{
+    public Sprinkles(IceCream iceCream) : base(iceCream) { }
+    public override string Description => base.Description + " with Sprinkles";
+    public override decimal CalculateCost() => base.CalculateCost() + 0.5m;
+}
+class ChocolateChips : IceCreamDecorator
+{
+    public ChocolateChips(IceCream iceCream) : base(iceCream) { }
+    public override string Description => base.Description + " with Chocolate Chips";
+    public override decimal CalculateCost() => base.CalculateCost() + 0.7m;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        IceCream iceCream = new ChocolateChips(new Sprinkles(new IceCream()));
+        Console.WriteLine(iceCream.Description);
+        Console.WriteLine($"Cost: {iceCream.CalculateCost():C}");
+        Console.ReadLine();
+    }
+}
---
+++ before_decorator_Design_Pattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DP_WithoutDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IceCream order = new IceCreamWithFruitMix();
+            IceCream order1 = new IceCreamWithSprinkles();
+            IceCream order2 = new IceCreamWithChocolateChips();
+
+            Console.WriteLine(order);
+            Console.WriteLine(order1);
+            Console.WriteLine(order2);
+
+            Console.ReadKey();
+        }
+    }
+    public class IceCream
+    {
+        public virtual string Description => "Ice cream";
+        public virtual decimal CalculateCost() => 3.5m;
+
+        public override string ToString()
+        {
+            return $"{Description} ({CalculateCost().ToString("C")})";
+        }
+    }
+
+    public class IceCreamWithSprinkles : IceCream
+    {
+        public override string Description => $"{base.Description} + Sprinkles";
+        public override decimal CalculateCost() => base.CalculateCost() + .25m;
+    }
+    public class IceCreamWithChocolateChips : IceCream
+    {
+        public override string Description => $"{base.Description} + Chocolate Chips";
+        public override decimal CalculateCost() => base.CalculateCost() + .45m;
+    }
+
+    public class IceCreamWithFruitMix : IceCream
+    {
+        public override string Description => $"{base.Description} + Fruit Mix";
+        public override decimal CalculateCost() => base.CalculateCost() + .60m;
+    }
+}
